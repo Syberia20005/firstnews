fd14504d53d7ee492a723cf9189fdad25534c9c3
Natalya Marchenko <syberia20005@gmail.com>
2016-04-29 23:33:27 +0300

    new dir

---
+++ myold/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: natasenka
+ * Date: 29.04.16
+ * Time: 19:06
+ */
+
+$myold  = 30;
+$diff   = 35 - $myold;
+$myname = 'Marchenko Natalya';
+
+if($myold > 35){
+    echo "Меня зовут: $myname и мне $myold";
+} else {
+    echo "Меня зовут: $myname и до 35 лет мне еще $diff лет";
+}
